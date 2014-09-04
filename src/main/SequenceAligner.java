29aee29c8271e913624508c85dfce0ad18fb5b13
Matt Mortimer <matthew.mortimer1@gmail.com>
2014-09-04 12:35:06 +1200

    Correct numbers be here

--- src/main/SequenceAligner.java
+++ src/main/SequenceAligner.java
@@ -6,23 +6,37 @@ public class SequenceAligner {
 	private int mCost = 1;
 	private int bCost = -1;
 
+	public int alpha(char x,char y){
+		return x == y ? mCost : bCost;
+	}
+
 
 	public void alignment(String x, String y){
 
 		int[][] matchings = initializeMatchingArray(x, y);
 
+		for(int j = 1; j < x.length()+1; j++){
+			for(int i = 1; i < y.length()+1; i++){
+				char a = x.charAt(j-1);
+				char b = y.charAt(i-1);
+				int optimal = Math.max(Math.max(alpha(a,b) + matchings[j-1][i-1], gCost + matchings[j-1][i]), gCost + matchings[j][i-1]);
+				matchings[j][i] = optimal;
+			}
+		}
+
+		System.out.println(matchings[x.length()][y.length()]);
+
 
 	}
 
 
-
 	private int[][] initializeMatchingArray(String x, String y) {
-		int[][] matchings = new int[x.length()][y.length()];
-		for(int i = 0; i < x.length(); i++){
-			matchings[i][0] = gCost;
+		int[][] matchings = new int[x.length()+1][y.length()+1];
+		for(int i = 0; i < x.length()+1; i++){
+			matchings[i][0] = i*gCost;
 		}
-		for(int i = 0; i < y.length(); i++){
-			matchings[0][i] = gCost;
+		for(int i = 0; i < y.length()+1; i++){
+			matchings[0][i] = i*gCost;
 		}
 
 		return matchings;
@@ -32,7 +46,7 @@ public class SequenceAligner {
 
 	public static void main(String[] args){
 		SequenceAligner sa = new SequenceAligner();
-		sa.alignment("good", "bad");
+		sa.alignment("GATCGGCAT", "CAATGTGAATC");
 
 	}
 }
